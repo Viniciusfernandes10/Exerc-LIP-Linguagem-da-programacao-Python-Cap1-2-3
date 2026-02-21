b20d15f7a0eb3b602a7c33f3a6bfbad7c4435357
Vinícius Fernandes <vinicius.fernandes.abreu120@gmail.com>
2026-02-21 14:11:06 -0300

    Add files via upload

---
+++ capitulo2/ex06.py
@@ -0,0 +1,10 @@
+def recebe_coordenadas(x, y):
+    coordenadas = (x, y)
+    return coordenadas
+
+x = float(input())
+y = float(input())
+
+coordenadas = recebe_coordenadas(x, y)
+
+print("Coordenadas:", coordenadas)
